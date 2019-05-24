4b18bd399e71a25f51f66a0d4454e1f12e1589fd
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2019-05-24 09:39:42 +0800

    Updates

---
+++ precache-manifest.025113eaedbaf8d6ecdc3c59f6600053.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72b3bc72c932840668f7f30bcea2535",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6bcc7462895365a28bad",
+    "url": "/weather-app/static/css/main.ab1fea10.chunk.css"
+  },
+  {
+    "revision": "002474f1f624bf107673",
+    "url": "/weather-app/static/js/2.52f54549.chunk.js"
+  },
+  {
+    "revision": "6bcc7462895365a28bad",
+    "url": "/weather-app/static/js/main.233940d8.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
